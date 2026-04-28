f113a116f74e164e7f148eae82c1e51939c8c5a8
sawyerstuebs-cmd <sawyerstuebs@gmail.com>
2026-04-27 19:09:11 -0500

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title('My First Streamlit App')
-st.write('Hello! This app is live on Streamlit Community Cloud.')
-name = st.text_input('Enter your name:')
-if name:
-st.write(f'Welcome, {name}!')
